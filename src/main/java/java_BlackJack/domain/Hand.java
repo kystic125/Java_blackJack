00d2dd87794d48c635776ff22af1ae138b7ac836
kystic125 <kystic125@naver.com>
2025-12-11 19:41:48 +0900

    refactor: Card와 점수 계산(ScoreCalculator)의 책임 분리 명확화
    - Hand는 패 공개만 담당

--- src/main/java/java_BlackJack/domain/Hand.java
+++ src/main/java/java_BlackJack/domain/Hand.java
@@ -5,32 +5,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Hand {
-
+    
     private final List<Card> cards = new ArrayList<>();
 
     public void addCard(Card card) {
         cards.add(card);
     }
 
-    public int calculateScore() {
-        int totalScore = cards.stream()
-                .mapToInt(Card::getScore)
-                .sum();
-
-        int aceCount = (int) cards.stream()
-                .filter(Card::isAce)
-                .count();
-
-        while (totalScore > 21 && aceCount >= 1) {
-            totalScore -= 10;
-            aceCount --;
-        }
-
-        return totalScore;
-    }
-
-    public List<Card> openPlayerCards() {
+    public List<Card> getCards() {
         return Collections.unmodifiableList(cards);
     }
 
+    public int getCardCount() {
+        return cards.size();
+    }
 }
